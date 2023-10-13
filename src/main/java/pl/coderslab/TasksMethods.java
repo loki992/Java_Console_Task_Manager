d383858582214752ccf50bd993da6fce93d00b01
loki992 <stempniak.adrian@gmail.com>
2023-10-13 10:45:00 +0200

    added basic tasks listing

--- src/main/java/pl/coderslab/TasksMethods.java
+++ src/main/java/pl/coderslab/TasksMethods.java
@@ -1,9 +1,23 @@
 package pl.coderslab;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class TasksMethods {
 
     public static void listingExistingTasks() {
-        System.out.println("listing existing tasks");
+        System.out.println();
+        Path path = Paths.get("tasks.csv");
+        try {
+            for (String line: Files.readAllLines(path)){
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println();
     }
     public static void addingNewTask (){
         System.out.println("adding new task");
