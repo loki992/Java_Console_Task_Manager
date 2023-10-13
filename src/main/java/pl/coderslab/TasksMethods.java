86c136cb7bc11cbb21092e885981f315255e5e88
loki992 <stempniak.adrian@gmail.com>
2023-10-13 11:05:23 +0200

    adding new task to the file simplified

--- src/main/java/pl/coderslab/TasksMethods.java
+++ src/main/java/pl/coderslab/TasksMethods.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Scanner;
+
+import static java.nio.file.StandardOpenOption.APPEND;
 
 public class TasksMethods {
 
@@ -11,7 +14,7 @@ public class TasksMethods {
         System.out.println();
         Path path = Paths.get("tasks.csv");
         try {
-            for (String line: Files.readAllLines(path)){
+            for (String line : Files.readAllLines(path)) {
                 System.out.println(line);
             }
         } catch (IOException e) {
@@ -19,11 +22,36 @@ public class TasksMethods {
         }
         System.out.println();
     }
-    public static void addingNewTask (){
+
+    public static void addingNewTask() {
         System.out.println("adding new task");
+        System.out.println("New task description:");
+        Scanner scannerAddingTask = new Scanner(System.in);
+        String newTaskDescription = scannerAddingTask.nextLine();
+        System.out.println("New task due date:");
+        String newTaskDate = scannerAddingTask.nextLine();
+        System.out.println("Is the new task important? [y]es / [n]o :");
+        boolean newTaskImportance;
+        String utilTaskImportance = scannerAddingTask.nextLine().trim().toLowerCase();
+        if (utilTaskImportance.equals("y")) {
+            newTaskImportance = true;
+        } else if (utilTaskImportance.equals("n")) {
+            newTaskImportance = false;
+        } else {
+            System.out.println("Wrong input, going back to menu");
+            return;
+        }
+        Path path = Paths.get("tasks.csv");
+        try {
+            Files.writeString(path, "\n" + newTaskDescription + ", " + newTaskDate + ", " + newTaskImportance, APPEND);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 
-    public static void removingExistingTask (){
+    public static void removingExistingTask() {
         System.out.println("removing existing task");
     }
 }
